37f82712bec3387032e78091dc48c5b32328ecbd
nasheomirro <76786181+nasheomirro@users.noreply.github.com>
2025-05-08 08:15:25 +0800

    added unplugin-icons and config

--- sites/theme-generator/src/app.d.ts
+++ sites/theme-generator/src/app.d.ts
@@ -1,3 +1,5 @@
+import 'unplugin-icons/types/svelte';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -10,4 +12,4 @@ declare global {
 	}
 }
 
-export {};
+export { };
