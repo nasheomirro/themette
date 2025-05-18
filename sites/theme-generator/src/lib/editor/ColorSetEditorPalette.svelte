cd94339ab7480e94fad68f6e2d9466e1b527afa9
nasheomirro <76786181+nasheomirro@users.noreply.github.com>
2025-05-18 10:42:27 +0800

    added manual bindings to color set editor

--- sites/theme-generator/src/lib/editor/ColorSetEditorPalette.svelte
+++ sites/theme-generator/src/lib/editor/ColorSetEditorPalette.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import chroma from "chroma-js";
   import type { ColorSet } from "$lib/app/types";
+  import { app } from "$lib/app/app.svelte";
+  import { colorShades } from "$lib/app/constants";
 
   type Props = {
     set: ColorSet;
@@ -11,18 +13,40 @@
 
 <div>
   <div class="space-y-5">
-    {#each [50, 500, 950] as const as shade}
+    {#each colorShades as shade}
       <div class="flex gap-4 items-center">
         <span class="text-xs w-6 text-center">
           {shade}
         </span>
-        <button
-          style="--bg: {set[shade]}; --c: {set.contrasts[shade]}"
-          class="text-sm bg-(--bg) text-(--c) p-1.5 rounded font-mono shadow"
-        >
-          {chroma(set[shade])}
-        </button>
+        <label class="w-7 h-7">
+          <span class="sr-only">Use native color picker to edit shade {shade}</span>
+          <input
+            type="color"
+            class="w-7 h-7 rounded shadow"
+            bind:value={() => chroma(set[shade]).hex(), (v) => app.updateColorSet(set.id, { [shade]: v })}
+          />
+        </label>
+        <label class="grow">
+          <span class="sr-only">color shade ({shade})</span>
+          <input class="input" bind:value={() => chroma(set[shade]).hex(), (v) => app.updateColorSet(set.id, { [shade]: v })} />
+        </label>
       </div>
     {/each}
   </div>
 </div>
+
+<style>
+  /* input reset */
+  input[type="color"]::-moz-color-swatch {
+    border: none;
+  }
+
+  input[type="color"]::-webkit-color-swatch-wrapper {
+    padding: 0;
+    border-radius: 0;
+  }
+
+  input[type="color"]::-webkit-color-swatch {
+    border: none;
+  }
+</style>
